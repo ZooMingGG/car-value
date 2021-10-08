62679e289b9c8a19a643e999ea176e800bb56c24
Volodymyr Lukashevych <vlukashe@softserveinc.com>
2021-10-08 13:05:32 +0300

    reafact: change user controller methods to use camel case

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -40,14 +40,14 @@ export class UsersController {
   }
 
   @Post('/signin')
-  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+  async signIn(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
     return user;
   }
 
   @Post('/signout')
-  signout(@Session() session: any) {
+  signOut(@Session() session: any) {
     session.userId = null;
   }
 
