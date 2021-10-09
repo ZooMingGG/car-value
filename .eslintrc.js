be3b179f656cdddea0027d5811d3cd408dd879a9
vbury <vbury@softserveinc.com>
2021-10-09 16:04:24 +0300

    add strict lint airbnb rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,69 @@
-module.exports = {};
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-plusplus': 'off',
+    'import/no-unresolved': 'error',
+    'prettier/prettier': 'error',
+    'arrow-parens': ['error', 'always'],
+    curly: ['error', 'all'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+    'no-useless-constructor': 'off',
+    'no-empty-function': [
+      'error',
+      {
+        allow: ['constructors'],
+      },
+    ],
+    'no-unused-vars': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'linebreak-style': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.spec.ts', '**/*.e2e-spec.ts'] },
+    ],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-member-accessibility': ['error'],
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
